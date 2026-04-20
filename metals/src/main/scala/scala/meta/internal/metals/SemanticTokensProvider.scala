1dab98ec3006f308a060129dede6e2c3cb555795
Asad Humayun <105254706+AsadHumayun@users.noreply.github.com>
2026-04-20 17:34:07 +0100

    feat: initial pass at twirl semantic tokens

--- metals/src/main/scala/scala/meta/internal/metals/SemanticTokensProvider.scala
+++ metals/src/main/scala/scala/meta/internal/metals/SemanticTokensProvider.scala
@@ -664,3 +664,98 @@ object EscapableString {
       case _ => None
     }
 }
+
+object TwirlSemanticTokensProvider {
+  /** This class stores delta information for a token. This contains the raw position of these tokens
+    * and not the relative delta tokens that are expected by the LSP (i.e. VSCode)
+    */
+  case class Position(
+    line: Int,
+    column: Int,
+  )
+
+  /** A constructed semantic token that contains no delta encoding as of right now. This token can be
+    * encoded by its source position later to delta encoding and ordered appropriately. This is
+    * largely because I am too stupid to figure out how to do everything in a single pass. Perhaps
+    * someone smarter can come up with something that is a less expensive solution, preferrably only
+    * one pass.
+    *
+    * @param length
+    *   The length of the token.
+    * @param tokenType
+    *   The type of the token (use the semantic tokens service to retrieve this value).
+    * @param tokenModifiers
+    *   Ditto.
+    * @param line
+    *   The source position of the line of the input text.
+    * @param column
+    *   Ditto.
+    */
+  case class SourceTwirlSemanticToken(
+    length: Int,
+    tokenType: Int,
+    tokenModifiers: Int,
+    line: Int,
+    column: Int,
+  ) {
+    def getSrcPos: Position = Position(line = line, column = column)
+
+    def deltaEncode(prevToken: DeltaEncodedTwirlSemanticToken): DeltaEncodedTwirlSemanticToken = {
+      println(s"deltaLine=[${prevToken.deltaLine}];deltaStart=[${prevToken.deltaStart}]")
+
+      val deltaLine = line - prevToken.deltaLine
+
+      // relative to 0 or the previous token’s start if they are on the same line
+      val deltaStart =
+        if (deltaLine == 0)
+          column - prevToken.deltaStart
+        else
+          column
+
+      DeltaEncodedTwirlSemanticToken(
+        deltaLine = deltaLine,
+        deltaStart = deltaStart,
+        length = length,
+        tokenType = tokenType,
+        tokenModifier = tokenModifiers,
+      )
+    }
+  }
+
+  /** A case class designed to make it easier to make it easier to construct and convert semantic
+    * tokens to/from their raw representation that is expected by LSP.
+    *
+    * @note
+    *   **THIS CLASS SHOULD NOT BE INSTANTIATED DIRECTLY.** The `encodeDelta(...)` method should be
+    *   used to convert a `SourceTwirlSemanticTokens` to a `DeltaEncodedTwirlSemanticTokens`.
+    */
+  case class DeltaEncodedTwirlSemanticToken(
+    deltaLine: Int,
+    deltaStart: Int,
+    length: Int,
+    tokenType: Int,
+    tokenModifier: Int,
+  ) {
+    def toList: List[Int] = List(deltaLine, deltaStart, length, tokenType, tokenModifier)
+
+    /** This method will return a `Position` using the delta values defined on the class. It should be
+      * made clear that this will not be the source position.
+      */
+    def toDeltaPos: Position = Position(deltaLine, deltaStart)
+  }
+
+  /** The idea is that this class will hold the state for the recursive logic when it comes to
+    * "jumping" between the ASTs.
+    *
+    * @param prevToken
+    *   The previous `SourceTwirlSemanticToken`
+    * @param tokens
+    *   All of the collected semantic tokens thus far
+    */
+  case class State(
+    prevToken: SourceTwirlSemanticToken,
+    tokens: Seq[SourceTwirlSemanticToken],
+  ) {
+    def getPrevPos: Position = prevToken.getSrcPos
+  }
+}
